febe8742549bba93f5f266db441d76656421848a
Kimtaesoo <taesoo2012@gmail.com>
2020-04-10 16:36:03 +0900

    Roll a ball 유니티 첫시간

---
+++ Roll a ball/Assets/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Text기능 시 필요
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Text mScoreText, mClearText;
+    private Rigidbody mRB;
+
+    [SerializeField]
+    private float mSpeed = 5;
+
+    [SerializeField]
+    private float mScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mScore = 0;
+        mRB = GetComponent<Rigidbody>();
+        //gameObject; transform; 스크립트가 붙어있는 물체
+        mScoreText.text = "Score: " + mScore.ToString();
+        mClearText.text = "";
+    }
+    public void AddScore()
+    {
+        mScore++;
+        mScoreText.text = "Score: "+ mScore.ToString();
+        if(mScore>=4)
+        {
+            mClearText.text = "Game Clear!";
+        }
+        //UI출력
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 axis = new Vector3(horizontal,0,vertical);
+        axis = axis.normalized*5;
+        axis.y = mRB.velocity.y;
+
+        //mRB.AddForce(axis);
+        mRB.velocity = axis;
+        //개별변경안됨
+        //float velY = mRB.velocity.y;
+        //mRB.velocity.y = velY;
+
+    }
+}
---
+++ Roll a ball/Assets/pickup.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 mTumble;
+    private Vector3 mRealTumble;
+    private Player mPlayer;
+    //// Start is called before the first frame update
+    void Start()
+    {
+        mRealTumble = mTumble * Time.fixedDeltaTime;
+        GameObject playerobj = GameObject.FindGameObjectWithTag("Player");//오브젝트 찾을때.
+        mPlayer = playerobj.GetComponent<Player>();//오브젝트 찾을때.
+        //mPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>(); 동일
+    }
+    private void FixedUpdate()
+    {
+        transform.Rotate(mRealTumble);
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    transform.Rotate(mTumble*Time.deltaTime); //프레임타임을 곱해줘 맞춰줌.
+    //}
+
+    private void OnTriggerEnter(Collider other) // 겹칩 둘중하나  Rigidbody필요  , 둘중 하나만 트리거면 된다. (or case)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false); //활성화 비활성화
+            mPlayer.AddScore();
+        }//Destroy(gameObject); 가비지 쌓임 안쓰는게 좋다.
+    }
+    //private void OnCollisionEnter(Collision collision) // 충돌 둘중하나 Rigidbody필요 , 둘다 콜리젼이여야된다.(and case)
+    //{
+        
+    //}
+}
