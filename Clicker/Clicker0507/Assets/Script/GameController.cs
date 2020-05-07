d410589696080d03ba9b1632a9d544a76b4189b8
Comts <taesoo2012@gmail.com>
2020-05-07 16:19:30 +0900

    Textmesh pro/ 광석이미지변경/게이지바 추가

---
+++ Clicker/Clicker0507/Assets/Script/GameController.cs
@@ -0,0 +1,85 @@
+﻿using System; // system에도 random이있음  UnityEngine.random사용
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+    private int mCurrentStage;
+
+    [SerializeField]
+    private double mProgressWeight = 1.08d;
+    private double mCurrentProgress;
+    private double mMaxProgress;
+
+    private double mTouchPower;
+
+    [SerializeField]
+    private Gem mCurrentGem;
+
+    [SerializeField]//임시
+    private GemPool mGemPool;
+
+    private void Awake()
+    {
+        
+        if(Instance==null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            //TODO Load Save Data
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        mCurrentStage = 0;
+        mMaxProgress = 10;
+        mTouchPower = 1;
+        mCurrentGem = mGemPool.GetFromPool(UnityEngine.Random.Range(0, Constants.TOTAL_GEM_COUNT));
+    }
+    
+    private void CalcNextStage()
+    {
+        mCurrentStage++;
+        //mMaxProgress = 10 * Mathf.Pow(mProgressWeight, mCurrentStage); //float
+        mMaxProgress = 10 * Math.Pow(mProgressWeight, mCurrentStage); //double  10* 1.08^n
+
+        mCurrentGem.gameObject.SetActive(false);
+        mCurrentGem = mGemPool.GetFromPool(UnityEngine.Random.Range(0, Constants.TOTAL_GEM_COUNT));
+        //Debug.Log("New Gem");
+        //Debug.Log(mMaxProgress);
+        mCurrentProgress = 0;
+    }
+
+    public void Touch()
+    {
+        if (mCurrentProgress >= mMaxProgress)
+        {
+            CalcNextStage();
+        }
+        else
+        {
+            mCurrentProgress += mTouchPower;
+            // 터치효과 적용 <- 원석의 작업 진행도 증가
+            float progress = (float)(mCurrentProgress / mMaxProgress); //이미지 fillamount값이 float
+            mCurrentGem.SetProgress(progress);
+            //Debug.Log(progress);
+            // 작업진행도에 따라 원석의 이미지 업데이트 -Gem
+        }
+        // 게이지바에 진행도 표시
+        UIController.Instance.ShowGaugeBar(mCurrentProgress, mMaxProgress);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Clicker/Clicker0507/Assets/Script/Paths.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Paths 
+{
+
+    public const string GEM_PREFAB = "Gem";
+
+}
+public static class Constants //나중에 분리.
+{
+    public const int TOTAL_GEM_COUNT = 3;
+}
