30039c4136550f7df03c2efe17bf2e6042438fd0
Comts <taesoo2012@gmail.com>
2020-05-22 14:45:29 +0900

    effect추가, 동료동작 쿨타임감소 추가

---
+++ Clicker/Clicker0522/Assets/Scripts/MainComponents/UIController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController Instance;
+    private static readonly int Movehash = Animator.StringToHash("Move");
+#pragma warning disable 0649
+    [SerializeField]
+    private GaugeBar mGaugeBar;
+
+    [SerializeField]
+    private Animator[] mWindowAnimArr;
+#pragma warning restore 0649
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ShowWindow(int id)
+    {
+        mWindowAnimArr[id].SetTrigger(Movehash);
+    }
+
+    public void ShowGaugeBar(double current, double max)
+    {
+        string progressStr = string.Format("{0} / {1}",
+                                            UnitSetter.GetUnitStr(current),
+                                            UnitSetter.GetUnitStr(max));
+        float progress = (float)(current / max);
+        mGaugeBar.ShowGaugeBar(progress, progressStr);
+    }
+
+    
+}
---
+++ Clicker/Clicker0522/Assets/Scripts/UIElement.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIElement : MonoBehaviour
+{
+#pragma warning disable 0649
+    [SerializeField]
+    private Image mIconImage;
+    [SerializeField]
+    private TextMeshProUGUI mTitleText, mLevelText, mContentsText, mCostText, mTenCostText;
+    [SerializeField]
+    private Button mButton, mTenUpButton;
+#pragma warning restore 0649
+    private Coroutine mButtonPopRoutine;
+    private int mClickCount;
+
+    private int mID;
+    public void Init(int id,
+                     Sprite icon, 
+                     string title, 
+                     string level,
+                     string contents,
+                     string cost,
+                     string tenCost,
+                     Delegates.TwoIntInVoidCallback callback)
+    {
+        mID = id;
+        mIconImage.sprite = icon;
+        mTitleText.text = title;
+        Refresh(level, contents, cost, tenCost);
+
+        mButton.onClick.AddListener(() => 
+        {
+            if(mButtonPopRoutine == null)
+            {
+                mClickCount = 0;
+                mButtonPopRoutine = StartCoroutine(ButtonPop());
+            }
+            mClickCount++;
+            callback(mID, 1);
+        });
+        mTenUpButton.onClick.AddListener(() => 
+        {
+            mClickCount++;
+            callback(mID, 10);
+        });
+    }
+
+    public void Refresh(string level, string contents, string cost, string tenCost)
+    {
+        mLevelText.text = level;
+        mContentsText.text = contents;
+        mCostText.text = cost;
+        mTenCostText.text = tenCost;
+    }
+
+    public void SetButtonActive(bool isActive)
+    {
+        mButton.interactable = isActive;
+    }
+
+    public void SetTenButtonActive(bool isActive)
+    {
+        mTenUpButton.interactable = isActive;
+    }
+
+    private IEnumerator ButtonPop()
+    {
+        WaitForSeconds pointOne = new WaitForSeconds(.1f);
+        float time = 3;
+        bool activeButton = false;
+        while(time > 0)
+        {
+            yield return pointOne;
+            time -= .1f;
+            if(mClickCount == 3)
+            {
+                activeButton = true && mTenUpButton.interactable;
+                mTenUpButton.gameObject.SetActive(activeButton);
+                break;
+            }
+        }
+
+        if(activeButton)
+        {
+            mClickCount = 0;
+            time = 3;
+            while (time > 0)
+            {
+                if (mClickCount > 0)
+                {
+                    time = 3;
+                    mClickCount = 0;
+                }
+                yield return pointOne;
+                time -= .1f;
+            }
+        }
+
+        mTenUpButton.gameObject.SetActive(false);
+        mButtonPopRoutine = null;
+    }
+}
