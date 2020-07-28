5682d3f6a917b3c21a593b540844854fae65af68
Comts <taesoo2012@gmail.com>
2020-07-28 16:47:52 +0900

    게임작업중,디자인수업

---
+++ NoTouch/NoTouch/Assets/Scrtipt/UIElement/AddStageUIElement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddStageUIElement : MonoBehaviour
+{
+    [SerializeField]
+    private Image mGemImage;
+    [SerializeField]
+    private Text mTitleText, mContentsText, mCostText;
+    [SerializeField]
+    private Button mAddButton;
+
+    private int mID;
+    public void Init(int id,
+                     Sprite icon,
+                     string title,
+                     string contents,
+                     string cost,
+                     Delegates.TwoIntInVoidCallback callback)
+    {
+        mID = id;
+        mGemImage.sprite = icon;
+        mTitleText.text = title;
+        Refresh(contents, cost);
+
+        mAddButton.onClick.AddListener(() =>
+        {
+            callback(mID, 1);
+        });
+    }
+    public void Refresh(string contents, string cost)
+    {
+        mContentsText.text = contents;
+        mCostText.text = cost;
+    }
+}
