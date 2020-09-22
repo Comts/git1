f2d191fefced5817c5a0aa6446859e4d911dfeed
Comts <taesoo2012@gmail.com>
2020-09-22 13:32:10 +0900

    stage이미지 변경완료, 두더지보상작업필요

---
+++ 광고,플러그인,빌드업/GoogleMobileAdsDemoScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+public class GoogleMobileAdsDemoScript : MonoBehaviour
+{
+    private RewardBasedVideoAd rewardBasedVideo;
+
+    public void Start()
+    {
+
+        rewardBasedVideo = RewardBasedVideoAd.Instance;
+
+        // Called when an ad request has successfully loaded.
+        rewardBasedVideo.OnAdLoaded += HandleRewardBasedVideoLoaded;
+        // Called when an ad request failed to load.
+        rewardBasedVideo.OnAdFailedToLoad += HandleRewardBasedVideoFailedToLoad;
+        // Called when an ad is shown.
+        rewardBasedVideo.OnAdOpening += HandleRewardBasedVideoOpened;
+        // Called when the ad starts to play.
+        rewardBasedVideo.OnAdStarted += HandleRewardBasedVideoStarted;
+        // Called when the user should be rewarded for watching a video.
+        rewardBasedVideo.OnAdRewarded += HandleRewardBasedVideoRewarded;
+        // Called when the ad is closed.
+        rewardBasedVideo.OnAdClosed += HandleRewardBasedVideoClosed;
+        // Called when the ad click caused the user to leave the application.
+        rewardBasedVideo.OnAdLeavingApplication += HandleRewardBasedVideoLeftApplication;
+
+
+        RequestRewardBasedVideo();
+    }
+
+    private void RequestRewardBasedVideo()
+    {
+#if UNITY_ANDROID
+           string adUnitId = "ca-app-pub-3940256099942544/5224354917"; //광고단위 ID
+#elif UNITY_IPHONE
+           string adUnitId = "ca-app-pub-3940256099942544/1712485313";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the rewarded video ad with the request.
+        rewardBasedVideo.LoadAd(request, adUnitId);
+    }
+
+    private void UserOptToWatchAd()
+    {
+        if (rewardBasedVideo.IsLoaded())
+        {
+            rewardBasedVideo.Show();
+        }
+        RequestRewardBasedVideo();
+    }
+}
+}
